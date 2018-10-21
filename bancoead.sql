eb9fcaa3dfa4365f9ace3a336b4090b26e7144f1
Gian Michel <gianveloxsi@gmail.com>
2018-10-21 20:00:04 -0300

    Atualizando arquivo sql e adicionando novas tabelas

--- bancoead.sql
+++ bancoead.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: 19-Out-2018 às 19:54
+-- Generation Time: 21-Out-2018 às 22:59
 -- Versão do servidor: 5.7.23
 -- versão do PHP: 7.2.10
 
@@ -31,18 +31,22 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `atividades`;
 CREATE TABLE IF NOT EXISTS `atividades` (
   `id_atividade` int(11) NOT NULL AUTO_INCREMENT,
-  `Atividade` varchar(500) COLLATE latin1_general_ci NOT NULL,
+  `Atividade` varchar(500) COLLATE latin1_general_ci DEFAULT NULL,
   `id_aluno` int(11) DEFAULT NULL,
   `resposta` varchar(500) COLLATE latin1_general_ci DEFAULT NULL,
+  `nota` float NOT NULL,
+  `valor` int(11) NOT NULL,
   PRIMARY KEY (`id_atividade`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 --
 -- Extraindo dados da tabela `atividades`
 --
 
-INSERT INTO `atividades` (`id_atividade`, `Atividade`, `id_aluno`, `resposta`) VALUES
-(2, 'Como é feito um teste?', NULL, NULL);
+INSERT INTO `atividades` (`id_atividade`, `Atividade`, `id_aluno`, `resposta`, `nota`, `valor`) VALUES
+(1, 'Como é feito um teste?', NULL, 'Testando', 3, 4),
+(2, 'Testando atividades?', NULL, 'Testes', 2, 3),
+(3, 'Como executar uma sequencia for()?', NULL, 'Testar', 1, 2);
 
 -- --------------------------------------------------------
 
@@ -62,7 +66,7 @@ CREATE TABLE IF NOT EXISTS `dados` (
   `nivel` enum('Aluno','Professor') COLLATE latin1_general_ci NOT NULL,
   `curso` varchar(100) COLLATE latin1_general_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 --
 -- Extraindo dados da tabela `dados`
@@ -70,7 +74,31 @@ CREATE TABLE IF NOT EXISTS `dados` (
 
 INSERT INTO `dados` (`id`, `usuario`, `nome`, `sobrenome`, `email`, `senha`, `confSenha`, `nivel`, `curso`) VALUES
 (1, 'gianvelox', 'Gian', 'Michel', 'gianveloxsi@gmail.com', 'a8e0a09388d92d2a17633e4e5adbdc66', 'a8e0a09388d92d2a17633e4e5adbdc66', 'Aluno', 'Sistemas de Informação'),
-(2, 'sandro', 'Sandro', 'Matias', 'sandro@unipam.edu.br', '2c40837dd4b97fd00a5a598a8a25426f', '2c40837dd4b97fd00a5a598a8a25426f', 'Professor', 'Sistemas de Informação');
+(2, 'sandro', 'Sandro', 'Matias', 'sandro@unipam.edu.br', '2c40837dd4b97fd00a5a598a8a25426f', '2c40837dd4b97fd00a5a598a8a25426f', 'Professor', 'Sistemas de Informação'),
+(3, 'jcarlos', 'Jose', 'Carlos', 'jcarlos_2013@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'e10adc3949ba59abbe56e057f20f883e', 'Aluno', 'Sistemas de Informação');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `materiais`
+--
+
+DROP TABLE IF EXISTS `materiais`;
+CREATE TABLE IF NOT EXISTS `materiais` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `arquivo` varchar(500) COLLATE latin1_general_ci NOT NULL,
+  `curso` varchar(200) COLLATE latin1_general_ci DEFAULT NULL,
+  `dataUpload` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+--
+-- Extraindo dados da tabela `materiais`
+--
+
+INSERT INTO `materiais` (`id`, `arquivo`, `curso`, `dataUpload`) VALUES
+(1, 'Poster.pdf', 'Sistemas', '2018-10-21'),
+(2, 'Teste.pdf', 'Sistemas', '2018-10-21');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
